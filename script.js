c1ad46c9c913bb5f6a3add64fbc07a1577e5767f
Hammers <gianluca.hammers@gmail.com>
2025-11-05 16:55:42 +0100

    Add cart functionality and update container ID for better structure

--- script.js
+++ script.js
@@ -40,14 +40,19 @@ const getData = function () {
         const btnDng = document.querySelectorAll(".card .btn-danger");
         btnDng.forEach((btn) => {
           btn.addEventListener("click", (e) => {
-            let thisBtn = e.target.closest(".card");
+            const thisBtn = e.target.closest(".card");
             thisBtn.style.display = "none";
           });
         });
         const btnPri = document.querySelectorAll(".card .btn-primary");
         btnPri.forEach((btn) => {
           btn.addEventListener("click", (e) => {
-            
+            const cestino = document.getElementById("cestino");
+            const createCart = document.createElement("img");
+            createCart.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"    fill="currentColor" class="bi bi-bag" viewBox="0 0 16 16">
+                        <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1m3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1z"/>
+                    </svg>`;
+            cestino.appendChild(createCart);
           });
         });
       });
